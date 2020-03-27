d814df79c57951263c8d3e9676e372ec3f595aed
Rashida Thomas <RThomas@pncl.local>
2020-03-26 21:29:49 -0400

    Code from Kirk Pdf

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,  {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class ToDoList extends Component {
+  constructor () {
+    super();
+    this.state ={
+      toDos: [],
+      currenttoDo: "",
+    }
+  }
+
+  //This is where we code our methods
+handleChange = event =>{
+  console.log(event.target.value)
+};
+addItem = event => {
+  console.log("addItem Method fired")
+};
+}
+
+render() {
+  return(
+    <div className="container">
+    <form onSubmit={this.addItem}>
+    <label htmlFor="taskName">Task Name:</label>
+    <input onChange={this.handleChange} name="taskName" type="text"placeholder="Add todo here!"/>
+    <button type="submit">Add Task</button>
+    </form>
     </div>
   );
 }
 
-export default App;
+
+export default ToDoList;
